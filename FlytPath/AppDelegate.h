468232d8c6fba6fe602f2422f76bb1ec9098c53e
Erik Bye <madclouds@gmail.com>
2014-09-12 10:07:52 -0700

    Initial Commit

---
+++ FlytPath/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlytPath
+//
+//  Created by Erik Bye on 9/12/14.
+//  Copyright (c) 2014 Erik Bye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
